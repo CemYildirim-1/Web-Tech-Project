005561daf91da48d23117a90ba21cdca9d173c72
AminKawy <Wanja.AbdElKawy@Student.HTW-Berlin.de>
2022-01-09 21:33:40 +0100

    added backend tests

--- src/test/java/htwberlin/webtech/NotizRestControllerTest.java
+++ src/test/java/htwberlin/webtech/NotizRestControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import HTWBERLIN.webtech.web.NotizbucheintragRestController;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -27,7 +28,7 @@ import static org.mockito.Mockito.doReturn;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-
+@ContextConfiguration
 @WebMvcTest(NotizbucheintragRestController.class)
 class NotizRestControllerTest {
 
